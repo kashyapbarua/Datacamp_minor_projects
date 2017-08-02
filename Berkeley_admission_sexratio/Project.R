2688a509711c33d945230846b87e3aa3563eda22
Kashyap Barua <xodiac642@gmail.com>
2017-08-02 16:02:08 +0530

    Male:Female Admit ratio according to department
    The conclusion comes out to be that the proportion of female admit is
    more than the males, department wise.

--- Berkeley_admission_sexratio/Project.R
+++ Berkeley_admission_sexratio/Project.R
@@ -13,3 +13,26 @@ ucb_counts
 # Spread the output across columns
 ucb_counts %>%
   spread(Admit, n)
+
+#Proportion of males admitted overall
+ucb_admit %>%
+  # Table of counts of admission status and gender
+  count(Gender, Admit) %>%
+  # Spread output across columns based on admission status
+  spread(Admit, n) %>%
+  # Create new variable
+  mutate(Perc_Admit = Admitted / (Admitted + Rejected))
+
+#Proportion of males admitted for each department
+
+# Table of counts of admission status and gender for each department
+admit_by_dept <- ucb_admit %>%
+  count(Dept, Gender, Admit) %>%
+  spread(Admit, n)
+
+# View result
+admit_by_dept
+
+# Percentage of those admitted to each department
+admit_by_dept %>%
+  mutate(Perc_Admit = Admitted / (Admitted + Rejected))
