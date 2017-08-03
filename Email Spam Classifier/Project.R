9af42630a80739f6c080b99b40b187f36a623622
Kashyap Barua <xodiac642@gmail.com>
2017-08-03 19:48:30 +0530

    Checking association with other variables

--- Email Spam Classifier/Project.R
+++ Email Spam Classifier/Project.R
@@ -16,3 +16,43 @@ email %>%
   mutate(log_num_char = log(num_char)) %>%
   ggplot(aes(x = spam, y = log_num_char)) +
   geom_boxplot()
+
+#NOTE: The median length of not-spam emails is greater than that of spam emails.
+
+#Relationship between exclaimation mark and whether or not a message is spam.
+
+# Compute center and spread for exclaim_mess by spam
+email %>%
+  group_by(spam) %>%
+  summarize(median(exclaim_mess),
+            IQR(exclaim_mess))
+
+# Create plot for spam and exclaim_mess
+email %>%
+  mutate(log_exclaim_mess = log(exclaim_mess + .01)) %>%
+  ggplot(aes(x = log_exclaim_mess)) +
+  geom_histogram() +
+  facet_wrap(~ spam)
+
+# Alternative plot: side-by-side box plots
+email %>%
+  mutate(log_exclaim_mess = log(exclaim_mess + .01)) %>%
+  ggplot(aes(x = 1, y = log_exclaim_mess)) +
+  geom_boxplot() +
+  facet_wrap(~ spam)
+
+# Alternative plot: Overlaid density plots
+email %>%
+  mutate(log_exclaim_mess = log(exclaim_mess + .01)) %>%
+  ggplot(aes(x = log_exclaim_mess, fill = spam)) +
+  geom_density(alpha = 0.3)
+
+#Image content and it's association with spam
+
+# Create plot of proportion of spam by image
+email %>%
+  mutate(has_image = image > 0) %>%
+  ggplot(aes(x = has_image, fill = spam)) +
+  geom_bar(position = "fill")
+
+#NOTE: An email without an image is more likely to be non-spam than spam.
