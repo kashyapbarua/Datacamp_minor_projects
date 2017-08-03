f5a1ad30970f7b62b144873007472f2a843969c0
Kashyap Barua <xodiac642@gmail.com>
2017-08-03 20:00:18 +0530

    Using other variables like number and images.

--- Email Spam Classifier/Project.R
+++ Email Spam Classifier/Project.R
@@ -56,3 +56,45 @@ email %>%
   geom_bar(position = "fill")
 
 #NOTE: An email without an image is more likely to be non-spam than spam.
+
+#do attached images count as attached files in this dataset?
+# Test if images count as attachments
+sum(email$image > email$attach)
+
+#Answering a question using piping example.
+#"Within non-spam emails, is the typical length of emails shorter for those that were sent to multiple people?"
+email %>%
+  filter(spam == "not-spam") %>%
+  group_by(to_multiple) %>%
+  summarize(median(num_char))
+#Creating chain for both the questions below.
+#1.For emails containing the word "dollar", does the typical spam email contain a greater number of occurrences of the word than the typical non-spam email? Create a summary statistic that answers this question.
+#2.If you encounter an email with greater than 10 occurrences of the word "dollar", is it more likely to be spam or not-spam? Create a barchart that answers this question.
+# Question 1
+email %>%
+  filter(dollar > 0) %>%
+  group_by(spam) %>%
+  summarize(median(dollar))
+
+# Question 2
+email %>%
+  filter(dollar > 10) %>%
+  ggplot(aes(x = spam)) +
+  geom_bar()
+
+#For illustrating relationships between categorical variables, you've seen
+
+#1. Faceted barcharts
+#2. Side-by-side barcharts
+#3. Stacked and normalized barcharts.
+
+#Checking the association with the number variable of whether being spam or not.
+
+# Reorder levels
+email$number <- factor(email$number, levels = c("none", "small", "big"))
+
+# Construct plot of number
+ggplot(email, aes(x = number)) +
+  geom_bar() +
+  facet_wrap(~ spam)
+#NOTE: Given that an email contains no number, it is more likely to be spam.
